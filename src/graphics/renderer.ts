bf66ca3de400130d622b3605b2d5a9578282907f
Arjan Draisma <arjan.draisma@philips.com>
2018-11-28 14:24:04 +0100

    now only re-rending when stuff happens

--- src/graphics/renderer.ts
+++ src/graphics/renderer.ts
@@ -1,12 +1,21 @@
 import { Chart } from '../chart/chart';
 import { Point } from '../common/point';
 import { ChartText} from './text';
+import { ChartObject } from '../chart/objects/object';
 
 export const MAX_ZOOM_LEVEL = 4;
 export const MIN_ZOOM_LEVEL = 1;
 
+interface RenderBoundingBox {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+}
+
 export class Renderer {
-    private animationFrameHandle: number;
+    // x, y, w, h
+    private renderBoundingBox: RenderBoundingBox;
 
     private cameraPosText: ChartText;
 
@@ -19,28 +28,23 @@ export class Renderer {
         this.cameraPosText = new ChartText('Test');
     }
 
-    private renderFrame = ()  => {
-        this.context.save();
-        this.context.clearRect(-this.cameraPos.x,  -this.cameraPos.y, this.context.canvas.clientWidth, this.context.canvas.clientHeight);
+    public repaint() {
+        this.context.clearRect(-this.cameraPos.x, -this.cameraPos.y, this.context.canvas.width, this.context.canvas.height);
         for (const object of this.chart.getObjects()) {
             object.draw(this.context, this.zoomLevel);
         }
-        this.cameraPosText.setText('x: ' + this.cameraPos.x + ' y: ' + this.cameraPos.y + ' z: ' + this.zoomLevel);
+        this.renderDebugInfo()
+    }
+
+    private renderDebugInfo() {
+        this.cameraPosText.setText('x: ' + this.cameraPos.x + ' y: ' + this.cameraPos.y 
+        + '\nz: ' + this.zoomLevel);
         this.cameraPosText.draw(new Point(10 - this.cameraPos.x, 600 - this.cameraPos.y), this.context, 1);
-        this.context.restore();
-        this.animationFrameHandle = requestAnimationFrame(this.renderFrame);
-    }
-
-    public start() {
-        requestAnimationFrame(this.renderFrame);
-    }
-
-    public stop() {
-        cancelAnimationFrame(this.animationFrameHandle);
     }
 
     private transformChanged() {
         this.context.setTransform(1, 0, 0, 1, this.cameraPos.x, this.cameraPos.y);
+        this.repaint();
     }
 
     public setCameraPos(pos: Point) {
@@ -62,6 +66,7 @@ export class Renderer {
         } else {
             this.zoomLevel = zoomLevel;
         }
+        this.transformChanged();
     }
 
     public getZoomLevel(): number {
@@ -69,7 +74,7 @@ export class Renderer {
     }
 
     public getViewCenter(): Point {
-        return new Point(0,0);
+        return new Point(this.cameraPos.x + this.context.canvas.width / 2, this.cameraPos.y + this.context.canvas.height / 2);
     }
 
     public getViewOffset(): Point {
